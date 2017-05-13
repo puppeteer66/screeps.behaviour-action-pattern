d17ed7d367112763bb131797eb863a75262b4d07
Mi Cha <michael_wark@web.de>
2017-05-13 02:56:01 +0200

    debug

--- room.js
+++ room.js
@@ -1256,6 +1256,9 @@ mod.validFields = function(roomName, minX, maxX, minY, maxY, checkWalkable = fal
 };
 // args = { spots: [{pos: RoomPosition, range:1}], checkWalkable: false, where: ()=>{}, roomName: abc ) }
 mod.fieldsInRange = function(args) {
+  if (args.roomName === 'E91S31') {
+    console.log(args.roomName + ' args.spots ' + args.spots.length);
+  }
     let plusRangeX = args.spots.map(spot => spot.pos.x + spot.range);
     let plusRangeY = args.spots.map(spot => spot.pos.y + spot.range);
     let minusRangeX = args.spots.map(spot => spot.pos.x - spot.range);
@@ -1266,6 +1269,7 @@ mod.fieldsInRange = function(args) {
     let maxY = Math.min(...plusRangeY);
     if (args.roomName === 'E91S31') {
       console.log(args.roomName + ' minX ' + minX + ' maxX ' + maxX + ' minY ' + minY + ' maxY ' + maxY + ' args.checkWalkable ' + args.checkWalkable);
+      console.log(args.roomName + ' plusRangeX ' + plusRangeX + ' plusRangeY ' + plusRangeY + ' minusRangeX ' + minusRangeX + ' minusRangeY ' + minusRangeY);
     }
     return Room.validFields(args.roomName, minX, maxX, minY, maxY, args.checkWalkable, args.where);
 };
