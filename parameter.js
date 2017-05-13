b9e22d6bdce0a9ec65d189ec3ad1ae8d2ebbf696
Mi Cha <michael_wark@web.de>
2017-05-13 19:06:41 +0200

    param

--- parameter.js
+++ parameter.js
@@ -151,7 +151,7 @@ let mod = {
     // function parameters: room. expected result: string
     CONTROLLER_SIGN_MESSAGE: `Puppetland`,
     CONTROLLER_SIGN_UPDATE: true, // Update sign message if user changes CONTROLLER_SIGN_MESSAGE
-    MINERS_AUTO_BUILD: true, // miners and remoteMiners will build their own containers if they are missing.
+    MINERS_AUTO_BUILD: false, // miners and remoteMiners will build their own containers if they are missing.
     MINER_WORK_THRESHOLD: 50, // how long to wait before a miner checks for repairs/construction sites nearby again
     REMOTE_HAULER: {
         ALLOW_OVER_CAPACITY: 2450, // Hauler capacity rounds up by MIN_WEIGHT, or this number value.
