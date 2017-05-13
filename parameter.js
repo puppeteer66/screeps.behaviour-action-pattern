90db39692aa01c198c2de154a1f48f35b10ecd9a
Mi Cha <michael_wark@web.de>
2017-05-13 16:08:18 +0200

    param

--- parameter.js
+++ parameter.js
@@ -28,7 +28,7 @@ let mod = {
     },
     TRAVELER_STUCK_TICKS: 2, // Number of ticks not moving to be considered stuck by the Traveler API
     TRAVELER_THRESHOLD: 5, // Average creep CPU usage/tick before warning about pathing cost, starts after 25 ticks
-    USE_UNBUILT_ROADS: true, // enabling this will set the pathing cost of road construction sites to that of roads
+    USE_UNBUILT_ROADS: false, // enabling this will set the pathing cost of road construction sites to that of roads
     GRAFANA: false, // track for Grafana data
     GRAFANA_INTERVAL: 3, // loops between Grafana tracking - No lower than 3.
     CENSUS_ANNOUNCEMENTS: true, // log birth and death
